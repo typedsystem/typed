db42855a989049998f05a854e320f93e2da9b7ae
yxm <yxm.dev@gmx.ie>
2026-06-03 15:58:53 -0300

    ref: globa refactor

--- typed/mods/meta/dependent.py
+++ typed/mods/meta/dependent.py
@@ -14,47 +14,41 @@ class TUPLE(TYPE):
     def __isterm__(typ, trm):
         from typed.mods.typesystem import typeof, issub, isterm
 
-        if  not isinstance(trm, tuple) and not issub(typeof(typeof(trm)), TUPLE):
+        if  not isinstance(trm, tuple) and not issub(typeof(trm, level=2), TUPLE):
             return False
 
         types = getattr(typ, '__types__', None)
         if types is not None:
-            for x in trm:
-                if not any(isterm(x, t) for t in types):
-                    return False
+            from typed.mods.init import every
+            return every(isterm(x, *types) for x in trm)
         return True
 
     def __issub__(typ, other):
-        from typed.mods.typesystem import issub
-        if type(other) is type(typ):
-            typ_types = getattr(typ, '__types__', None)
-            other_types = getattr(other, '__types__', None)
-            if typ_types is None and other_types is not None:
+        from typed.mods.typesystem import issub, isequiv, typeof
+        if isequiv(typeof(other), typeof(typ)):
+            types = getattr(typ, '__types__', None)
+            others = getattr(other, '__types__', None)
+            if types is None and others is not None:
                 return False
-            if other_types is None:
+            if others is None:
                 return True
-            for t1 in typ_types:
-                if not any(issub(t1, t2) for t2 in other_types):
-                    return False
-            return True
+            from typed.mods.init import every
+            return every(issub(t, *others) for t in types)
         return False
 
     def __call__(typ, *types, typesystem=None):
         from typed.mods.typesystem import names
-        from typed.mods.init import TYPESYSTEM
-        if typesystem is None:
-            typesystem = TYPESYSTEM
+        from typed.mods.check import resolve
 
-        types = set(types)
-        if typesystem.is_restrictive:
-            from typed.mods.check import check
-            check.typesystem(*types, typesystem=typesystem)
+        typesystem = resolve.typesystem.entity(typesystem)
+        from typed.mods.check import check
+        check.every.ismember(types, typesystem)
 
         name = f"Tuple({names(*types)})" if types else "Tuple"
 
         return TUPLE(name, (typ,), {
-            "is_type": True,
-            "__typesystems__": [typesystem],
+            "__kind__": "type",
+            "__typesystems__": {typesystem,},
             "__display__": name,
             "__types__": types,
         })
@@ -62,8 +56,8 @@ class TUPLE(TYPE):
     from typed.mods.err import NotDefined
     from typed.mods.init import TYPESYSTEM
 
-    is_meta = True
-    __typesystems__ = [TYPESYSTEM]
+    __kind__ = "meta"
+    __typesystems__ = {TYPESYSTEM, }
     __display__ = "TUPLE"
     __null__ = NotDefined
     __builtin__ = NotDefined
@@ -73,58 +67,51 @@ class LIST(TYPE):
     """
     The metatype of lists.
 
-    kindof(LIST)    is meta
-    typeof(LIST)    is UNIVERSE(1)
-    isterm(T, LIST) iff isinstance(T, list) and elements match types, or issub(typeof(typeof(T)), LIST)
-    nullof(LIST)    is NotDefined
-    builtin(LIST)   is NotDefined
+    kindof(LIST)    is  meta
+    typeof(LIST)    is  UNIVERSE(1)
+    isterm(T, LIST) iff issub(typof(T), LIST)
+    nullof(LIST)    is  NotDefined
+    builtin(LIST)   is  NotDefined
     """
 
     def __isterm__(typ, trm):
         from typed.mods.typesystem import typeof, issub, isterm
 
-        if not isinstance(trm, list) and not issub(typeof(typeof(trm)), LIST):
+        if not isinstance(trm, list) and not issub(typeof(trm, level=2), LIST):
             return False
 
         types = getattr(typ, '__types__', None)
         if types is not None:
-            for x in trm:
-                if not any(isterm(x, t) for t in types):
-                    return False
+            from typed.mods.init import every
+            return every(isterm(x, *types) for x in trm)
         return True
 
     def __issub__(typ, other):
-        from typed.mods.typesystem import issub
-        if type(other) is type(typ):
-            typ_types = getattr(typ, '__types__', None)
-            other_types = getattr(other, '__types__', None)
-            if typ_types is None and other_types is not None:
+        from typed.mods.typesystem import issub, isequiv, typeof
+        if isequiv(typeof(other), typeof(typ)):
+            types  = getattr(typ, '__types__', None)
+            others = getattr(other, '__types__', None)
+            if types is None and others is not None:
                 return False
-            if other_types is None:
+            if others is None:
                 return True
-            for t1 in typ_types:
-                if not any(issub(t1, t2) for t2 in other_types):
-                    return False
-            return True
+            from typed.mods.init import every
+            return every(issub(t, *others) for t in types)
         return False
 
     def __call__(typ, *types, typesystem=None):
+        from typed.mods.check import check, resolve
         from typed.mods.typesystem import names
-        if typesystem is None:
-            from typed.mods.init import TYPESYSTEM
-            typesystem = TYPESYSTEM
+        typesystem = resolve.typesystem.entity(typesystem)
 
         types = set(types)
-        if typesystem.is_restrictive:
-            for t in types:
-                if t not in typesystem.__types__:
-                    raise TypeError(f"Type {t} not in typesystem.__types__")
+        check.every.ismember(types, typesystem)
 
         name = f"List({names(*types)})" if types else "List"
 
         return LIST(name, (typ,), {
-            "is_type": True,
-            "__typesystems__": [typesystem],
+            "__kind__": "type",
+            "__typesystems__": {typesystem,},
             "__display__": name,
             "__types__": types,
         })
@@ -132,8 +119,8 @@ class LIST(TYPE):
     from typed.mods.init import TYPESYSTEM
     from typed.mods.err import NotDefined
 
-    is_meta = True
-    __typesystems__ = [TYPESYSTEM]
+    __kind__ = "meta"
+    __typesystems__ = {TYPESYSTEM, }
     __display__ = "LIST"
     __null__ = NotDefined
     __builtin__ = NotDefined
@@ -143,62 +130,59 @@ class SET(TYPE):
     """
     The metatype of sets.
 
-    kindof(SET)    is meta
-    typeof(SET)    is UNIVERSE(1)
-    isterm(T, SET) iff isinstance(T, set) and elements match types, or issub(typeof(typeof(T)), SET)
-    nullof(SET)    is NotDefined
-    builtin(SET)   is NotDefined
+    kindof(SET)    is  meta
+    typeof(SET)    is  UNIVERSE(1)
+    isterm(T, SET) iff issub(typeof(T), SET)
+    nullof(SET)    is  NotDefined
+    builtin(SET)   is  NotDefined
     """
     def __isterm__(typ, trm):
         from typed.mods.typesystem import typeof, issub, isterm
 
-        if not isinstance(trm, set) and not issub(typeof(typeof(trm)), SET):
+        if not isinstance(trm, set) and not issub(typeof(trm, level=2), SET):
             return False
 
         types = getattr(typ, '__types__', None)
         if types is not None:
-            for x in trm:
-                if not any(isterm(x, t) for t in types):
-                    return False
+            from typed.mods.init import every
+            return every(isterm(x, *types) for x in trm)
         return True
 
     def __issub__(typ, other):
-        from typed.mods.typesystem import issub
-        if type(other) is type(typ):
-            typ_types = getattr(typ, '__types__', None)
-            other_types = getattr(other, '__types__', None)
-            if typ_types is None and other_types is not None:
+        from typed.mods.typesystem import issub, isequiv, typeof
+        if isequiv(typeof(other), typeof(typ)):
+            types  = getattr(typ, '__types__', None)
+            others = getattr(other, '__types__', None)
+            if types is None and others is not None:
                 return False
-            if other_types is None:
+            if others is None:
                 return True
-            for t1 in typ_types:
-                if not any(issub(t1, t2) for t2 in other_types):
-                    return False
-            return True
+            from typed.mods.init import every
+            return every(issub(t, *others) for t in types)
         return False
 
+
     def __call__(typ, *types, typesystem=None):
+        from typed.mods.check import check, resolve
         from typed.mods.typesystem import names
-        if typesystem is None:
-            typesystem = TYPESYSTEM
+        typesystem = resolve.typesystem.entity(typesystem)
 
         types = set(types)
-        if typesystem.is_restrictive:
-            for t in types:
-                if t not in typesystem.__types__:
-                    raise TypeError(f"Type {t} not in typesystem.__types__")
+        check.every.ismember(types, typesystem)
 
-        name = f"Set({names(*types)})" if types else "Set()"
+        name = f"Set({names(*types)})" if types else "Set"
 
-        return type.__new__(typ.__class__, name, (typ,), {
+        return SET(name, (typ,), {
+            "__kind__": "type",
             "__display__": name,
             "__types__": types,
-            "__typesystems__": [typesystem],
-            "is_type": True
+            "__typesystems__": {typesystem,},
         })
 
-    is_meta = True
-    __typesystems__ = [TYPESYSTEM]
+    from typed.mods.init import TYPESYSTEM
+    from typed.mods.err import NotDefined
+    __kind__ = "meta"
+    __typesystems__ = {TYPESYSTEM,}
     __type__ = UNIVERSE_1
     __display__ = "SET"
     __null__ = NotDefined
@@ -209,11 +193,11 @@ class DICT(TYPE):
     """
     The metatype of dictionaries.
 
-    kindof(DICT)    is meta
-    typeof(DICT)    is UNIVERSE(1)
-    isterm(T, DICT) iff isinstance(T, dict) and its keys/values match types, or issub(typeof(typeof(T)), DICT)
-    nullof(DICT)    is NotDefined
-    builtin(DICT)   is NotDefined
+    kindof(DICT)    is  meta
+    typeof(DICT)    is  UNIVERSE(1)
+    isterm(T, DICT) iff issub(typeof(T), DICT)
+    nullof(DICT)    is  NotDefined
+    builtin(DICT)   is  NotDefined
     """
     def __isterm__(typ, trm):
         from typed.mods.typesystem import typeof, issub, isterm
@@ -225,88 +209,85 @@ class DICT(TYPE):
         key_type = getattr(typ, "__key_type__", None)
 
         if types is not None:
-            for v in trm.values():
-                if not any(isterm(v, t) for t in types):
-                    return False
+            from typed.mods.init import every
+            if not every(isterm(v, *types) for v in trm.values()):
+                return False
 
         if key_type is not None:
-            for k in trm.keys():
-                if not isterm(k, key_type):
-                    return False
+            from typed.mods.init import every
+            if not every(isterm(k, key_type) for k in trm.keys()):
+                return False
 
         return True
 
     def __issub__(typ, other):
         from typed.mods.typesystem import issub
         if type(other) is type(typ):
-            typ_types = getattr(typ, '__types__', None)
-            other_types = getattr(other, '__types__', None)
+            types = getattr(typ, '__types__', None)
+            others = getattr(other, '__types__', None)
             typ_key = getattr(typ, '__key_type__', None)
             other_key = getattr(other, '__key_type__', None)
 
-            if typ_types is None and other_types is not None:
+            if types is None and others is not None:
                 return False
             if typ_key is None and other_key is not None:
                 return False
 
-            if typ_types is not None and other_types is not None:
-                for t1 in typ_types:
-                    if not any(issub(t1, t2) for t2 in other_types):
-                        return False
+            if types is not None and others is not None:
+                from typed.mods.init import every
+                if not every(issub(t, *others) for t in types):
+                    return False
 
             if typ_key is not None and other_key is not None:
                 if not issub(typ_key, other_key):
                     return False
-
             return True
         return False
 
     def __call__(typ, *types, key=None, typesystem=None):
-        from typed.mods.typesystem import names, nameof
-        if typesystem is None:
-            typesystem = TYPESYSTEM
+        from typed.mods.check import check, resolve
+        from typed.mods.typesystem import names
+        typesystem = resolve.typesystem.entity(typesystem)
 
         types = set(types)
-        if typesystem.is_restrictive:
-            for t in types:
-                if t not in typesystem.__types__:
-                    raise TypeError(f"Type {t} not in typesystem.__types__")
-            if key is not None and key not in typesystem.__types__:
-                raise TypeError(f"Type {key} not in typesystem.__types__")
-
         if key is not None:
-            name = f"Dict({names(*types)}, key={nameof(key)})" if types else f"Dict(key={nameof(key)})"
+            name = f"Dict({names(*types)}, key={typesystem.nameof(key)})" if types else f"Dict(key={typesystem.nameof(key)})"
         else:
             name = f"Dict({names(*types)})" if types else "Dict"
 
-        return TYPE(typ.__class__, name, (typ,), {
+        check.ismember(key, typesystem)
+        check.every.ismember(types, typesystem)
+
+        return DICT(name, (typ,), {
+            "__kind__": "type",
             "__display__": name,
             "__types__": types,
             "__key_type__": key,
-            "__typesystems__": [typesystem],
-            "is_type": True
+            "__typesystems__": {typesystem, },
         })
 
-    is_meta = True
-    __typesystems__ = [TYPESYSTEM]
+    from typed.mods.init import TYPESYSTEM
+    from typed.mods.err import NotDefined
+
+    __kind__ = "meta"
+    __typesystems__ = {TYPESYSTEM,}
     __type__ = UNIVERSE_1
     __display__ = "DICT"
     __null__ = NotDefined
     __builtin__ = NotDefined
 
-
 class EXTENSIONAL(TYPE):
-    def __isterm__(typ, trm):
+    def __isterm__(typ, trm, typesystem=None):
         quantifier = getattr(typ, "__quantifier__", None)
-        types = tuple(getattr(typ, "__types__", (typ,)))
-        from typed.mods.core import isterm
+        types = set(getattr(typ, "__types__", (typ,)))
+        from typed.mods.typesystem import isterm
         return isterm(trm, *types, quantifier=quantifier)
 
     def __issub__(typ, other):
         quantifier = getattr(typ, "__quantifier__", None)
         types = (t for t in getattr(typ, "__types__", (typ,)))
         others = (o for o in getattr(other, "__types__", (other,)))
-        from typed.mods.core import issub
+        from typed.mods.typesystem import issub
         if getattr(other, "is_extensional", False):
             return quantifier(issub(o, *types, quantifier=quantifier) for o in others)
         return issub(other, *types, quantifier=quantifier)
